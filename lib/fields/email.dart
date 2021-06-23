f50ac84b1f258798ce74d78ac41e14d25b168d05
OussemaZaier <oussamaweldlos@gmail.com>
2021-06-23 17:04:02 +0100

    Initial commit

---
+++ lib/fields/email.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Email extends StatefulWidget {
+  const Email({Key? key}) : super(key: key);
+
+  @override
+  _EmailState createState() => _EmailState();
+}
+
+class _EmailState extends State<Email> {
+  final emailController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    emailController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'can\'t be empty';
+          }
+        },
+        controller: emailController,
+        decoration: InputDecoration(
+          hintText: 'emailEXAMPLE@email.com',
+          labelText: 'E-Mail',
+          prefixIcon: Icon(Icons.email),
+          suffixIcon: emailController.text.isEmpty
+              ? Container(
+                  width: 0,
+                )
+              : IconButton(
+                  onPressed: () {
+                    emailController.clear();
+                  },
+                  icon: Icon(Icons.close),
+                ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        keyboardType: TextInputType.emailAddress,
+        textInputAction: TextInputAction.next,
+      ),
+    );
+  }
+}
